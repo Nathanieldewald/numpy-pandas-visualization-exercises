f7e25a71e88f493785b0291d035cf9be378f0de5
Nathaniel Dewald <Dewald.nathaniel@gmail.com>
2023-03-28 11:19:13 -0500

    completed pandas_series.ipynb

--- pandas_series.py
+++ pandas_series.py
@@ -1,39 +1,39 @@
-import pandas as pd
-import numpy as np
-from pydataset import data
-
-
-a = ["kiwi", "mango", "strawberry", "pineapple", "gala apple", "honeycrisp apple", "tomato", "watermelon", "honeydew", "kiwi", "kiwi", "kiwi", "mango", "blueberry", "blackberry", "gooseberry", "papaya"]
-
-
-'''
-1.Determine the number of elements in fruits.
-
-2.Output only the index from fruits.
-
-3.Output only the values from fruits.
-
-4.Confirm the data type of the values in fruits.
-
-5.Output only the first five values from fruits. Output the last three values. Output two random values from fruits.
-
-6.Run the .describe() on fruits to see what information it returns when called on a Series with string values.
-
-7.Run the code necessary to produce only the unique string values from fruits.
-
-8.Determine how many times each unique string value occurs in fruits.
-
-9.Determine the string value that occurs most frequently in fruits.
-
-10.Determine the string value that occurs least frequently in fruits.
-'''
-
-
-question1 = a
-
-answer1 = 0
-print("1.Determine the number of elements in fruits.", answer1)
-
-
-my_list = [2, 3, 5]
-print(type(my_list))
+# import pandas as pd
+# import numpy as np
+# from pydataset import data
+#
+#
+# a = ["kiwi", "mango", "strawberry", "pineapple", "gala apple", "honeycrisp apple", "tomato", "watermelon", "honeydew", "kiwi", "kiwi", "kiwi", "mango", "blueberry", "blackberry", "gooseberry", "papaya"]
+#
+#
+# '''
+# 1.Determine the number of elements in fruits.
+#
+# 2.Output only the index from fruits.
+#
+# 3.Output only the values from fruits.
+#
+# 4.Confirm the data type of the values in fruits.
+#
+# 5.Output only the first five values from fruits. Output the last three values. Output two random values from fruits.
+#
+# 6.Run the .describe() on fruits to see what information it returns when called on a Series with string values.
+#
+# 7.Run the code necessary to produce only the unique string values from fruits.
+#
+# 8.Determine how many times each unique string value occurs in fruits.
+#
+# 9.Determine the string value that occurs most frequently in fruits.
+#
+# 10.Determine the string value that occurs least frequently in fruits.
+# '''
+#
+#
+# question1 = a
+#
+# answer1 = 0
+# print("1.Determine the number of elements in fruits.", answer1)
+#
+#
+# my_list = [2, 3, 5]
+# print(type(my_list))
